fd742265236e79ac7465febd585993368163f8c6
Stefan Klinkert <s.klinkert@gmail.com>
2024-08-12 17:59:55 +0200

    complete Data Structures Lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,6 @@
+
+import ipdb
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +20,51 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = []
+    for i in range(len(spicy_foods)):
+        names.append(spicy_foods[i]["name"])
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    names = []
+    for i in range(len(spicy_foods)):
+        if spicy_foods[i]["heat_level"] > 5:
+            names.append(spicy_foods[i])
+    return names
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for i in range(len(spicy_foods)):
+        heat = spicy_foods[i]['heat_level']
+        my_string = heat * "🌶"
+        print(f"{spicy_foods[i]['name']} ({spicy_foods[i]['cuisine']}) | Heat Level: " + my_string)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for i in range(len(spicy_foods)):
+        if spicy_foods[i]['cuisine'] == cuisine:
+            return spicy_foods[i]
+        else:
+            pass
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    print_spicy_foods(get_spiciest_foods(spicy_foods))
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat = []
+    for i in range(len(spicy_foods)):
+        heat_food = spicy_foods[i]['heat_level']
+        heat.append(heat_food)
+    avg = int(sum(heat) / (len(spicy_foods)))
+    return avg
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    new_list = spicy_foods
+    new_list.append(spicy_food)
+    return new_list
+
+'''new_list = create_spicy_food(spicy_foods, {
+        'name': 'Griot',
+        'cuisine': 'Haitian',
+        'heat_level': 10,
+    })
+    '''
+#ipdb.set_trace()
